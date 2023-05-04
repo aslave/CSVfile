a40c0b2dda45763f7e68baaa87f9f3ec31ff33df
sharmila <98534919+skallya2@users.noreply.github.com>
2023-05-04 17:28:45 +0530

    Update demo.py

--- create/demo.py
+++ create/demo.py
@@ -1,2 +1,45 @@
+import csv
 
+date = []
+
+def tranform_row(row):
+  EMP_ID = line[0]
+  EMP_NAME = int(line[1])
+  Experience = line[2]
+  
+  return [
+
+         EMP_ID,
+
+         EMP_NAME,
+
+         Experience,
+
+        None,
+
+         ]
+
+#Read CSV File
+
+with open('team.csv','rb') as f:
+     reader = csv.reader(f)
+     header = reader.next()
+     for line in reader:
+        if not line: continue 
+        data.append(transform_row(line))
+
+#write CSV file
+
+with open('output.csv','w') as f:
+     writer = csv.writer(f, delimiter=',')
+     writer.writerow([
+     'EMPID',
+     'EMPNAME',
+     'Experience',
+     None,
+
+])
+
+#write our data to the file 
+writer.writerows(data)
 
